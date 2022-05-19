6ff0b6d7556df6bf433cf337c37279d419806c3e
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-05-19 20:52:29 +0300

    Created repository

---
+++ Library/Library.DataAccess/Data/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataAccess.Data
+{
+    public interface IRepository
+    {
+        public Task<IEnumerable<AutorRead>> GetAuthorsAll();
+    }
+}
---
+++ Library/Library.DataAccess/Data/MemoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Library.DataAccess.Entities;
+using Library.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataAccess.Data
+{
+    public class MemoryRepository : IRepository
+    {
+        public Task<IEnumerable<AutorRead>> GetAuthorsAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
