c48015e8c30ca3fc01f2d076163a6ddccc4d441e
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-05-20 23:00:39 +0300

    added books reducer

---
+++ reactclient/src/redux/reducers/booksReducer.ts
@@ -0,0 +1,35 @@
+import { BooksAction, BooksActionTypes, IBooksState } from "../../types/books";
+
+const initialState: IBooksState = {
+  books: [],
+  loading: false,
+  error: null
+}
+
+export const booksReducer = (state = initialState, action: BooksAction): IBooksState => {
+  switch(action.type) {
+    case BooksActionTypes.FETCH_BOOKS:
+      return {
+        loading: true,
+        error: null,
+        books: []
+      };
+
+    case BooksActionTypes.FETCH_BOOKS_SUCCESS:
+      return {
+        loading: false,
+        error: null,
+        books: action.payload
+      };
+
+    case BooksActionTypes.FETCH_BOOKS_ERROR:
+      return {
+        loading: false,
+        error: action.payload,
+        books: []
+      }
+
+    default:
+      return state;
+  }
+}
