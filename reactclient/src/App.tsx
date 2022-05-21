4eab13c9bf3fd23674b520c98e8518dfef92f3eb
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-05-21 10:53:17 +0300

    Added route in client

--- reactclient/src/App.tsx
+++ reactclient/src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
 import Authors from './components/Authors/Authors';
-import Books from './components/Books/Books'; 
+import Books from './components/Books/Books';
 
 function App() {
   return (
     <div className="App">
-      <Books />
+      <Link to="/authors">Авторы</Link>
+      <Link to="/books">Книги</Link>
+      
+      <Routes>
+        <Route path='/' element={<Authors />} />
+        <Route path='/authors' element={<Authors />} />
+        <Route path='/books' element={<Books />} />
+      </Routes>
     </div>
   );
 }
