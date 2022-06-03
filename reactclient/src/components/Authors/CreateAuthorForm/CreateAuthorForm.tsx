fc64f42958482108b83c89b3c0295c3b55615bc0
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-06-03 23:57:21 +0300

    Added validation form at create author

--- reactclient/src/components/Authors/CreateAuthorForm/CreateAuthorForm.tsx
+++ reactclient/src/components/Authors/CreateAuthorForm/CreateAuthorForm.tsx
@@ -1,61 +1,112 @@
 import React from "react";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { Formik } from "formik";
+import * as yup from 'yup';
 import { useTypesSelector } from "../../../hooks/useTypeSelector";
 import { fetchCreateAuthor } from "../../../redux/action-creators/createAuthor";
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css';
 
 const CreateAuthorForm: React.FC = () => {
-  const {author, loading, error} = useTypesSelector(state => state.createAuthor);
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
-  const [dateOfBirth, setDateOfBirth] = useState<Date | null>(new Date());
-
   const dispatch = useDispatch();
 
-  const changeHandlerFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFirstName(e.target.value);
-  }
-
-  const changeHandlerLastName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLastName(e.target.value);
-  }
-
-  const clickHandler = (e: React.MouseEvent) => {
-   dispatch(fetchCreateAuthor({
-      firstName,
-      lastName,
-      dateOfBirth: {
-        year: dateOfBirth?.getFullYear(),
-        month: dateOfBirth?.getMonth() + 1,
-        day: dateOfBirth?.getDate()
-      }
-    }));
-  }
+  const validationsSchema = yup.object().shape({
+    firstName: yup.string().required('Обязательно'),
+    lastName: yup.string().required('Обязательно'),
+    description: yup.string()
+  });
 
   return (
     <div className="container">
-      <h1 className="col d-flex flex-column justify-content-center align-items-center">Add author</h1>
-      <div className="form-group">
-        <input type="text" value={firstName} onChange={changeHandlerFirstName}
-          className="form-control" placeholder="first name" />
-      </div>
-      <br />
-      <div className="form-group">
-        <input type="text" value={lastName} onChange={changeHandlerLastName}
-        className="form-control" placeholder="last name" />
-      </div>
-      <br />
-      <div className="form-group">
-        <DatePicker className="form-control" selected={dateOfBirth}
-        onChange={setDateOfBirth} dateFormat="MM/dd/yyyy" />
-      </div>
-      <br />
-      <div className="form-group">
-        <button type="button" className="btn btn-success form-control"
-        placeholder="date of birth" onClick={clickHandler}> Create </button>
-      </div>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          description: "",
+          dateOfBirth: new Date()
+        }}
+        validateOnBlur
+        onSubmit={(values) => {
+          const firstName = values.firstName;
+          const lastName = values.lastName;
+          const description = values.description;
+          const dateOfBirth = {
+              year: values.dateOfBirth?.getFullYear(),
+              month: values.dateOfBirth?.getMonth() + 1,
+              day: values.dateOfBirth?.getDate()
+          }
+
+          dispatch(fetchCreateAuthor({
+            firstName,
+            lastName,
+            description,
+            dateOfBirth
+          }));
+        }}
+        validationSchema={validationsSchema}
+      >
+        {({values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
+          <div className="container">
+            <div className="form-group">
+              <input
+              type={"text"}
+              name={`firstName`}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.firstName}
+              className="form-control"
+              placeholder="first name"
+              />
+              {touched.firstName && errors.firstName && <p className={"error"}>{errors.firstName}</p>}
+            </div>
+            <br />
+            <div className="form-group">
+              <input
+              type={"text"}
+              name={`lastName`}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.lastName}
+              className="form-control"
+              placeholder="last name"
+              />
+              {touched.lastName && errors.lastName && <p className={"error"}>{errors.lastName}</p>}
+            </div>
+            <br />
+
+            <div className="form-group">
+              <input
+                type={"text"}
+                name={`description`}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.description}
+                className="form-control"
+                placeholder="description"
+              />
+            </div>
+            <br />
+            
+            <div className="form-group">
+              <DatePicker
+              className="form-control"
+              selected={values.dateOfBirth}
+              onChange={handleBlur}
+              dateFormat="MM/dd/yyyy" />
+            </div>
+            <br />
+
+            <div className="form-group">
+              <button
+              type="submit"
+              className="btn btn-success form-control"
+              placeholder="date of birth"
+              disabled={!isValid && !dirty}
+              onClick={handleSubmit}> Create </button>
+            </div>
+          </div>
+        )}
+      </Formik>
     </div>
   );
 }
